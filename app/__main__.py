25dce345a92327483c4231de9f6ed656226301c9
minebyte <petrlesha@gmail.com>
2025-01-15 20:51:18 +0100

    expose to the internet

--- app/__main__.py
+++ app/__main__.py
@@ -3,4 +3,4 @@ import uvicorn
 from app.core.config import settings
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", reload=True, log_config=settings.LOGGING_CONFIG)
+    uvicorn.run("app.main:app", reload=True, host="0.0.0.0", log_config=settings.LOGGING_CONFIG)
